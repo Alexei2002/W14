19d09e95cb26acf3bce4432655ad3bd79ac16ad3
Alex <konyukhov94@list.ru>
2023-08-06 22:28:05 +0300

    take 3

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,65 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //задача№1
+        System.out.println("example 1");
+        double dog1 = 8.0;
+        var cat1 = 3.6;
+        int paper1 = 763789;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
+        //задача №2
+        System.out.println("example 2");
+        var dog2 = dog1 -4;
+        var cat2 = cat1 -4;
+        var paper2 = paper1 -4;
+        System.out.println(dog2);
+        System.out.println(cat2);
+        System.out.println(paper2);
+        //Задача№3
+        System.out.println("example 3");
+        var dog3 = dog2 -3.5;
+        var cat3 = cat2 -1.6;
+        var paper3 = paper2 -7639;
+        System.out.println(dog3);
+        System.out.println(cat3);
+        System.out.println(paper3);
+        //задача № 4
+        System.out.println("example 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        // задача №5)
+        System.out.println("example 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        // задача №6
+        System.out.println("example 6");
+        var boxer2 = 78.2;
+        var boxer1 = 82.7;
+        System.out.println("Sum of weighrs: " + (boxer1 + boxer2));
+        // задача №7
+        System.out.println("example 7");
+        var diff1 = boxer1 - boxer2;
+        var diff2 = boxer1 % boxer2;
+        System.out.println("Diff1: " + diff1);
+        System.out.println("Diff2: " + diff2);
+        // задача № 8
+        System.out.println("example 8");
+        var hours = 640;
+        var hoursPerEmployee = 8;
+        var noueberOfEmployees = hours / hoursPerEmployee;
+        System.out.println(" Всего работников в компании - " + noueberOfEmployees + "человек");
+        noueberOfEmployees = noueberOfEmployees + 94;
+        hours = noueberOfEmployees + hoursPerEmployee;
+        System.out.println("Если в компании работает " + noueberOfEmployees + "человек, то всего " + hours + " часов работы может быть поделено между сотрудниками».");
     }
 }
